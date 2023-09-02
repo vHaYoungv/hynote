1f652c4330844a63fb9e92b3375587e2859257e8
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-09-02 13:24:34 +0900

    feat(Posts): Service 추가

---
+++ src/main/java/com/hy/hynote/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.hy.hynote.service.posts;
+
+import com.hy.hynote.domain.posts.PostsRepository;
+import com.hy.hynote.web.dto.PostsSaveRequestDto;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
